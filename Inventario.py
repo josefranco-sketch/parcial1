1d2eb048a1aa09f60037ea229d4f860471ee3c64
Jose Franco <jose.franco@ufm.edu>
2026-06-17 23:07:55 -0600

    Def 1, calcular_valor_total

--- Inventario.py
+++ Inventario.py
@@ -1,1 +1,7 @@
-print("Hola mundo")
+import pandas as pd #importar la librería pandas para usar en este proyecto y renombrarla
+
+#Definir función calcular inventario (será llamada después)
+
+def calcular_valor_total(inventario):
+    inventario["valor_total"] = inventario ["cantidad"] * inventario["precio"] #Toma la columna cantidad y precio y las multiplica
+    return inventario
